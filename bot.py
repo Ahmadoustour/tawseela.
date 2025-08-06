bdd77a99ce1d1687087a4202ffa5822f290c8422
Ahmad Oustour <ahmadalmeraabi@gmail.com>
2025-08-06 15:01:49 +0000

    تحديث شامل

--- bot.py
+++ bot.py
@@ -58,7 +58,7 @@ class APIError(Exception):
     def __init__(self, message, status_code=None):
         self.status_code = status_code
         super().__init__(message)
-
+#
 class APIConnectionError(Exception):
     def __init__(self, message, original_exception=None):
         self.original = original_exception
